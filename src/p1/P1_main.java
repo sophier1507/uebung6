5ebe1ea396ec8befc0762227880a5d598eb49b2e
sophier1507 <sophieroesgen@gmail.com>
2025-11-24 12:33:06 +0100

    p1

---
+++ src/p1/P1_main.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class P1_main {
+
+	public static void main(String[] args) {
+		System.out.println(squared(-3));
+		}
+
+	public static int squared(int n) {
+		n++;
+		return n * n;
+	}
+
+}
